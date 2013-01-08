14a7b3bc21bfc6ae748cdab8838e0e784f0f5d62
alexwh <alexwh@users.noreply.github.com>
2013-01-08 17:28:51 +0000

    use nokogiri to build xml & remove reading basehost from file

--- r53.rb
+++ r53.rb
@@ -1,81 +1,85 @@
 #!/usr/bin/env ruby
-require 'net/http'
-require 'time'
-require 'openssl'
-require 'base64'
-
-def getip
-  return Net::HTTP.get('ifconfig.me', '/ip')
-end
+require "net/http"
+require "time"
+require "openssl"
+require "base64"
+require "nokogiri"
 
 def get_info(type)
-  file_name = 'acc.info'
+  file_name = 'aws.key'
   info = Array.new
-  f = File.new(file_name, 'r')
+  f = File.new(file_name)
   f.read.each_line {|line| info << line.chomp}
   f.close
 
   return info[0] if type == 'secret' # your AWS secret
   return info[1] if type == 'access' # your AWS access key ID
   return info[2] if type == 'hostedzone' # your hosted zone ID
-  return info[3] if type == 'basehost' # your base domain, prepended with a dot (e.g. .example.com) @TODO: change that
 end
 
-@hostname = ARGV[0]
-@basehost = get_info('basehost')
+def build_xml
+  hostname = ARGV[0]
+  ttl = '60'
+  ttl = ARGV[1] unless ARGV[1].to_s.empty?
+  pubip = Net::HTTP.get('icanhazip.com', '/') # ifconfig.me was very slow - switched
 
-# @TODO: finish formatting of ip to delete and maybe use an xml builder
+  builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+    xml.ChangeResourceRecordSetsRequest('xmlns' => 'https://route53.amazonaws.com/doc/2012-02-29/') {
+      xml.ChangeBatch {
+        xml.Comment "Automatic DNS update for #{hostname} from **fill previous ip** to #{pubip}"
+        xml.Changes {
+          xml.Change {
+            xml.Action "DELETE"
+            xml.ResourceRecordSet {
+              xml.Name hostname
+              xml.Type "A"
+              xml.TTL ttl
 
-@xml = "<?xml version='1.0' encoding='UTF-8'?>
-<ChangeResourceRecordSetsRequest xmlns='https://route53.amazonaws.com/doc/2012-02-29/'>
-   <ChangeBatch>
-      <Comment>
-         Automatic DNS update for #{@hostname} from to #{getip}
-      </Comment>
-      <Changes>
-          <Change>
-              <Action>DELETE</Action>
-              <ResourceRecordSet>
-                 <Name>#{@hostname}#{@basehost}</Name>
-                 <Type>A</Type>
-                 <TTL>60</TTL>
-                 <ResourceRecords>
-                    <ResourceRecord>
-                       <Value>xxx.xxx.xxx.xxx</Value>
-                    </ResourceRecord>
-                 </ResourceRecords>
-              </ResourceRecordSet>
-           </Change>
-           <Change>
-            <Action>CREATE</Action>
-            <ResourceRecordSet>
-               <Name>#{@hostname}#{@basehost}</Name>
-               <Type>A</Type>
-               <TTL>60</TTL>
-               <ResourceRecords>
-                  <ResourceRecord>
-                     <Value>#{getip}</Value>
-                  </ResourceRecord>
-               </ResourceRecords>
-            </ResourceRecordSet>
-         </Change>
-      </Changes>
-   </ChangeBatch>
-</ChangeResourceRecordSetsRequest>"
+              xml.ResourceRecords {
+                xml.ResourceRecord {
+                  xml.Value "previous ip"
+                }
+              }
+            }
+          }
+          xml.Change {
+            xml.Action "CREATE"
+            xml.ResourceRecordSet {
+              xml.Name hostname
+              xml.Type "A"
+              xml.TTL "60"
 
-def request
+              xml.ResourceRecords {
+                xml.ResourceRecord {
+                  xml.Value pubip
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  end
+  return builder.to_xml
+end
+
+
+def request(type)
   baseurl = 'https://route53.amazonaws.com'
-  apipath = "/2012-02-29/hostedzone/#{get_info('hostedzone')}/"
-  uri = URI(baseurl+apipath+'rrset') # seems dirty
+  apipath = "/2012-02-29/hostedzone/#{get_info('hostedzone')}/rrset"
+  uri = URI(baseurl+apipath) # seems dirty
 
   digest = OpenSSL::Digest::Digest.new('sha256')
   time_data = Time.new.rfc822 # declared in var in case it takes >1 sec
   digested_data = OpenSSL::HMAC.digest(digest, get_info('secret'), time_data)
   signature = Base64::encode64(digested_data).chomp
 
-  request = Net::HTTP::Post.new(uri.path)
-  request.body = @xml
-  request.content_type = 'text/xml'
+  if type == 'update'
+    request = Net::HTTP::Post.new(uri.path)
+    request.body = build_xml
+    request.content_type = 'text/xml'
+  end
+
 
   request['Date'] = time_data
   request['X-Amzn-Authorization'] = "AWS3-HTTPS AWSAccessKeyId=#{get_info('access')},Algorithm=Hmac#{digest.name},Signature=#{signature}"
@@ -87,6 +91,6 @@ def request
   end
 end
 
-# puts request
+# puts request('update')
 
-puts "usage: #{$0} host (to be prepended to #{@basehost})" if ARGV.empty? # @TODO: stop this from being slow as shit
+puts "usage: #{$0} host [ttl]" if ARGV.empty?
