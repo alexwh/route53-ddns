777b08a95817df0188ffb4987b1bae8108fc5db9
alexwh <alexwh@users.noreply.github.com>
2013-01-09 22:03:57 +0000

    start on looping

--- r53.rb
+++ r53.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+
+if ARGV.empty?
+puts "usage: #{File.basename(__FILE__)} host [update interval] [ttl] [keyfile]" # change to $0 if files are split
+exit # prevents long running requests stalling
+end
+
 require "net/http"
 require "time"
 require "openssl"
@@ -7,10 +13,12 @@ require "nokogiri"
 
 @api_path = "https://route53.amazonaws.com/2012-02-29" # maybe a constant instead?
 @hostname = ARGV[0]
+@upd_int = 30
+@upd_int = ARGV[1].to_i unless ARGV[1].to_s.empty?
 
 def get_info(type)
   file_name = 'aws.key'
-  file_name = ARGV[2] unless ARGV[2].to_s.empty?
+  file_name = ARGV[3] unless ARGV[3].to_s.empty?
   info = Array.new
   f = File.new(file_name)
   f.read.each_line {|line| info << line.chomp}
@@ -41,7 +49,7 @@ end
 
 def build_xml
   ttl = '60'
-  ttl = ARGV[1] unless ARGV[1].to_s.empty?
+  ttl = ARGV[2] unless ARGV[2].to_s.empty?
   pubip = Net::HTTP.get('icanhazip.com', '/') # ifconfig.me was very slow - switched
 
   builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
@@ -98,19 +106,6 @@ def update
   @change_id = xml.css("Id").to_s.sub(/<Id>/,"").sub(/<\/Id>/,"") # ugly. unfortunatley xml.xpath("//Id()") refuses to work (is it html only?)
 end
 
-def fetch_old_ip
-  @uri = URI(@api_path+"/hostedzone/#{get_info('hostedzone')}/rrset")
-  params = {name: @hostname, type: "A", maxitems: "1"}
-  @uri.query = URI.encode_www_form(params)
-  @request = Net::HTTP::Get.new(@uri.request_uri)
-
-  set_date_auth
-  send_request
-
-  xml = Nokogiri::XML(send_request)
-  @old_ip = xml.css("Value").to_s.sub(/<Value>/,"").sub(/<\/Value>/,"")
-end
-
 def status
   @uri = URI(@api_path+"#{@change_id}")
   @request = Net::HTTP::Get.new(@uri.path)
@@ -122,14 +117,35 @@ def status
   return xml.css("Status").to_s.sub(/<Status>/,"").sub(/<\/Status>/,"")
 end
 
-puts "usage: #{$0} host [ttl] [keyfile]" if ARGV.empty?
+def fetch_aws_ip
+  @uri = URI(@api_path+"/hostedzone/#{get_info('hostedzone')}/rrset")
+  @uri.query = URI.encode_www_form({name: @hostname, type: "A", maxitems: "1"})
+  @request = Net::HTTP::Get.new(@uri.request_uri)
 
-fetch_old_ip
-update
+  set_date_auth
+  send_request
 
-while status == 'PENDING'
-  puts status
-  sleep 15
+  xml = Nokogiri::XML(send_request)
+  @old_ip = xml.css("Value").to_s.sub(/<Value>/,"").sub(/<\/Value>/,"")
 end
 
-puts "Done!" if status == 'INSYNC'
+
+# broke everything while prettying this up and had to revert
+
+# fetch_aws_ip
+# awsip = @old_ip
+
+# loop do
+#   currentip = Net::HTTP.get('icanhazip.com', '/')
+#   if currentip != awsip
+#     update
+
+#     while status == 'PENDING'
+#       puts "Pending..."
+#       sleep 30
+#     end
+#     puts "Done! Status #{status}. Will check again in #{@upd_int} mins"
+#     awsip = currentip
+#   end
+#   sleep @upd_int*60
+# end
