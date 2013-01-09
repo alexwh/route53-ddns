30d49a6d2d6913d3564363be209b07ad40a953ec
alexwh <alexwh@users.noreply.github.com>
2013-01-09 18:24:44 +0000

    fix fetching old ip (therefore updates) and loop on status updates till INSYNC

--- r53.rb
+++ r53.rb
@@ -5,7 +5,8 @@ require "openssl"
 require "base64"
 require "nokogiri"
 
-@apipath = "https://route53.amazonaws.com/2012-02-29/" # maybe a constant instead?
+@api_path = "https://route53.amazonaws.com/2012-02-29" # maybe a constant instead?
+@hostname = ARGV[0]
 
 def get_info(type)
   file_name = 'aws.key'
@@ -21,7 +22,6 @@ def get_info(type)
 end
 
 def build_xml
-  hostname = ARGV[0]
   ttl = '60'
   ttl = ARGV[1] unless ARGV[1].to_s.empty?
   pubip = Net::HTTP.get('icanhazip.com', '/') # ifconfig.me was very slow - switched
@@ -29,18 +29,18 @@ def build_xml
   builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
     xml.ChangeResourceRecordSetsRequest('xmlns' => 'https://route53.amazonaws.com/doc/2012-02-29/') {
       xml.ChangeBatch {
-        xml.Comment "Automatic DNS update for #{hostname} from **fill previous ip** to #{pubip}"
+        xml.Comment "Automatic DNS update for #{@hostname} from #{@old_ip} to #{pubip}"
         xml.Changes {
           xml.Change {
             xml.Action "DELETE"
             xml.ResourceRecordSet {
-              xml.Name hostname
+              xml.Name @hostname
               xml.Type "A"
               xml.TTL ttl
 
               xml.ResourceRecords {
                 xml.ResourceRecord {
-                  xml.Value fetch
+                  xml.Value @old_ip
                 }
               }
             }
@@ -48,9 +48,9 @@ def build_xml
           xml.Change {
             xml.Action "CREATE"
             xml.ResourceRecordSet {
-              xml.Name hostname
+              xml.Name @hostname
               xml.Type "A"
-              xml.TTL "60"
+              xml.TTL ttl
 
               xml.ResourceRecords {
                 xml.ResourceRecord {
@@ -72,7 +72,7 @@ def gensig
   return Base64::encode64(digested_data).chomp
 end
 
-def sendrequest
+def send_request
   Net::HTTP.start(@uri.host, @uri.port, use_ssl: @uri.scheme == 'https') do |http|
     response = http.request(@request)
     return response.body
@@ -80,44 +80,57 @@ def sendrequest
 end
 
 def update
-  @uri = URI(@apipath+"hostedzone/#{get_info('hostedzone')}/rrset") # + seems dirty - some sort of concat method instead?
+  @uri = URI(@api_path+"/hostedzone/#{get_info('hostedzone')}/rrset") # + seems dirty - some sort of concat method instead?
 
-  @request = Net::HTTP::Post.new(uri.path)
+  @request = Net::HTTP::Post.new(@uri.path)
   @request.body = build_xml
   @request.content_type = 'text/xml'
 
   @request['Date'] = Time.new.rfc822
   @request['X-Amzn-Authorization'] = "AWS3-HTTPS AWSAccessKeyId=#{get_info('access')},Algorithm=HmacSHA256,Signature=#{gensig}"
 
-  sendrequest
+  send_request
+
+  xml = Nokogiri::XML(send_request)
+  @change_id = xml.css("Id").to_s.sub(/<Id>/,"").sub(/<\/Id>/,"") # ugly. unfortunatley xml.xpath("//Id()") refuses to work (html only?)
 end
 
-def fetch
-  @uri = URI(@apipath+"hostedzone/#{get_info('hostedzone')}/rrset")
-  @request = Net::HTTP::Get.new(@uri.path)
+def fetch_old_ip
+  @uri = URI(@api_path+"/hostedzone/#{get_info('hostedzone')}/rrset")
+  params = {name: @hostname, type: "A", maxitems: "1"}
+  @uri.query = URI.encode_www_form(params)
+  @request = Net::HTTP::Get.new(@uri.request_uri)
   @request['Date'] = Time.new.rfc822
   @request['X-Amzn-Authorization'] = "AWS3-HTTPS AWSAccessKeyId=#{get_info('access')},Algorithm=HmacSHA256,Signature=#{gensig}"
 
 
-  sendrequest
+  send_request
 
-  # failed attempts at parsing xml when there's no namespaces =\
-  # xml = Nokogiri::XML(sendrequest)
-  # names = xml.css("ResourceRecordSets ResourceRecordSet Name")
-  # puts names
-  # each_line {|line| puts line if line.include?(ARGV[0])}
-  # puts xml
+  xml = Nokogiri::XML(send_request)
+  @old_ip = xml.css("Value").to_s.sub(/<Value>/,"").sub(/<\/Value>/,"")
 end
 
 def status
-  @uri = URI(@apipath+"change/#{@changeid}")
+  @uri = URI(@api_path+"#{@change_id}")
   @request = Net::HTTP::Get.new(@uri.path)
   @request['Date'] = Time.new.rfc822
   @request['X-Amzn-Authorization'] = "AWS3-HTTPS AWSAccessKeyId=#{get_info('access')},Algorithm=HmacSHA256,Signature=#{gensig}"
 
-  sendrequest
+  send_request
+
+  xml = Nokogiri::XML(send_request)
+  return xml.css("Status").to_s.sub(/<Status>/,"").sub(/<\/Status>/,"")
 end
 
-puts fetch
+
+fetch_old_ip
+update
+
+while status == 'PENDING'
+  puts status
+  sleep 10
+end
+
+puts "Done!" if status == 'INSYNC'
 
 puts "usage: #{$0} host [ttl] [keyfile]" if ARGV.empty?
